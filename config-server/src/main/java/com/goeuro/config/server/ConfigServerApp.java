658ec9dc7436eebeeb5e4776e5fa6d062935983d
Adriano Oliveira <adriano.silva@goeuro.com>
2018-11-18 09:08:49 +0100

    client and server are working properly

--- config-server/src/main/java/com/goeuro/config/server/ConfigServerApp.java
+++ config-server/src/main/java/com/goeuro/config/server/ConfigServerApp.java
@@ -2,10 +2,11 @@ package com.goeuro.config.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.web.bind.annotation.RestController;
 
+@EnableConfigServer
 @SpringBootApplication
-@RestController
 public class ConfigServerApp {
     public static void main(String[] arguments) {
         SpringApplication.run(ConfigServerApp.class, arguments);
