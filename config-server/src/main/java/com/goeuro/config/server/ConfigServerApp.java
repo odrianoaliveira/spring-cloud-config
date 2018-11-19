3969ee510e40f676f55487aedcb3b730e7b1a223
Adriano Oliveira <adriano.silva@goeuro.com>
2018-11-19 15:38:10 +0100

    removing uncesessary config

--- config-server/src/main/java/com/goeuro/config/server/ConfigServerApp.java
+++ config-server/src/main/java/com/goeuro/config/server/ConfigServerApp.java
@@ -3,21 +3,12 @@ package com.goeuro.config.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.monitor.GithubPropertyPathNotificationExtractor;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
 @EnableConfigServer
 @EnableAutoConfiguration
 @SpringBootApplication
 public class ConfigServerApp {
-
-    @Bean
-    public GithubPropertyPathNotificationExtractor githubPropertyPathNotificationExtractor() {
-        return new GithubPropertyPathNotificationExtractor();
-    }
 
     public static void main(String[] arguments) {
         SpringApplication.run(ConfigServerApp.class, arguments);
