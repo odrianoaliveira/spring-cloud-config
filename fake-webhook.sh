0285cc654f35edd56e8837d07c52ec75265d9b3a
Adriano Oliveira <adriano.silva@goeuro.com>
2018-11-19 17:12:28 +0100

    organizing config folders

--- fake-webhook.sh
+++ fake-webhook.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # to install it, type: brew install httpie
-http --json POST http://root:s3cr3t@localhost:8888/monitor X-Github-Event:"push" commits:='[{"modified": ["config-client-development.yml"] }]'
+http --json POST http://root:s3cr3t@localhost:8888/monitor X-Github-Event:"push" commits:='[{"modified": ["application.yml"] }]'
