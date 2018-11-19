50c72fee4e6acd71096102746946f2d622cda94f
Adriano Oliveira <adriano.silva@goeuro.com>
2018-11-19 15:11:53 +0100

    rename config files

--- config-client/src/main/java/com/goeuro/config/client/ConfigClientApp.java
+++ config-client/src/main/java/com/goeuro/config/client/ConfigClientApp.java
@@ -5,7 +5,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 @RefreshScope // <= auto refresh configuration
 @RestController
